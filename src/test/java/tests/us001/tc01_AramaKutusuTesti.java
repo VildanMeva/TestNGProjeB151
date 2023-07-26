2a1134e2e59535eeef5691f50d570c7fd82d68f3
Vildan Ince <vildanince29@gmail.com>
2023-07-26 14:04:04 +0200

    US001_V2

--- src/test/java/tests/us001/tc01_AramaKutusuTesti.java
+++ src/test/java/tests/us001/tc01_AramaKutusuTesti.java
@@ -12,6 +12,6 @@ public class tc01_AramaKutusuTesti {
         Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
         Page page = new Page();
         page.aramaKutusu.sendKeys("iphohe", Keys.ENTER );
-
+       Driver.closeDriver();
     }
 }
